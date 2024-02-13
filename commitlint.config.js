9ad0d1efc05d72e113eb7c99769c4cb0afd56143
Íñigo Marquínez Prado <25435858+inigomarquinez@users.noreply.github.com>
2024-02-13 09:04:35 +0100

    fix: commitlint patter to skip release message

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,10 @@
-module.exports = { extends: ['@commitlint/config-conventional'] }
+const automaticCommitPattern = /^chore\(release\):.*\[skip ci]/;
+
+module.exports = {
+  extends: [
+    '@commitlint/config-conventional'
+  ],
+  ignores: [
+    commitMsg => automaticCommitPattern.test(commitMsg)
+  ]
+};
